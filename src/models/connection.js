25fcdc1d51a3acc7a2676e40fa8da9cc6c2c1f1b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-29 09:42:18 -0300

    configura connection.js

---
+++ src/models/connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const DB_NAME = 'Cookmaster';
+
+let connection = null;
+
+module.exports = async () => {
+  if (!connection) {
+    connection = (await MongoClient.connection(
+      URL,
+      OPTIONS,
+    )).db(DB_NAME);
+  }
+  
+  return connection;
+};
