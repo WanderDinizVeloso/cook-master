b1f5c49a88fa791aa59c44dd0648d03618aced8d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-29 10:44:01 -0300

    cria middleware error

---
+++ src/controllers/middlewares/error.js
@@ -0,0 +1,15 @@
+const { StatusCodes } = require('http-status-codes');
+
+module.exports = async (err, _req, res, _next) => {
+  const { status = null, message } = err;
+
+  if (status) {
+    return res
+    .status(status)
+    .json({ message });
+  }
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ message: 'sorry, internal error' });
+};
