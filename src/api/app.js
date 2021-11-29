c758ae5a4e1a77accd5948c9b9d6170256b82c9f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-29 11:01:57 -0300

    implementa middleware error e root.js no app.js

--- src/api/app.js
+++ src/api/app.js
@@ -3,14 +3,21 @@ const express = require('express');
 
 require('dotenv').config();
 
+const root = require('../controllers/routes/root');
+const { error } = require('../controllers/middlewares');
+
 const app = express();
 
 app.use(json());
+
+app.use(root);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(error);
 
 module.exports = app;
