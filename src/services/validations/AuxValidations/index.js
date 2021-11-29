d2198b666000dc6c667fdd2dafa90978e7f79c33
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-29 17:39:37 -0300

    cria index.js da pasta AuxValidations

---
+++ src/services/validations/AuxValidations/index.js
@@ -0,0 +1,17 @@
+const isEmailConflict = require('./isEmailConflict');
+const isFilledFilelds = require('./IsFilledFields');
+const isIncorrectFields = require('./isIncorrectFields');
+const isIncorrectToken = require('./isIncorrectToken');
+const isRecipeFound = require('./isRecipeFound');
+const isRequiredFild = require('./IsRequiredField');
+const isTokenFound = require('./isTokenFound');
+
+module.exports = {
+  isEmailConflict,
+  isFilledFilelds,
+  isIncorrectFields,
+  isIncorrectToken,
+  isRecipeFound,
+  isRequiredFild,
+  isTokenFound,
+};
