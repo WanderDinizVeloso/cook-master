a88ef32c3916f9242ece10198d2fd55a64d83fd2
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-30 16:04:19 -0300

    cria upload.js na pasta middlewares

---
+++ src/controllers/middlewares/upload.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
+  },
+  filename: (req, file, callback) => {
+    const { id } = req.params;
+
+    callback(null, `${id}.jpeg`);
+  },
+});
+
+module.exports = multer({ storage });
